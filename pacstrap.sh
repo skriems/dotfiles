7f05b8890315f2ac4dd5c562d02ca618728a68db
Sebastian Kriems <bastoberto@gmx.de>
2019-02-10 12:09:38 +0100

    install maven and german libreoffice packages

--- pacstrap.sh
+++ pacstrap.sh
@@ -16,10 +16,10 @@ pactrap /mnt \
     nodejs npm \
     rust rustup rust-racer \
     go dep \
-    jdk-openjdk \
+    jre-openjdk maven \
     jre8-openjdk java-openjfx \ # needed for MediathekView
     # gnome gnome-software-packagekit-plugin gnome-tweak-tool \
-    chromium owncloud-client vlc libreoffice-fresh \
+    chromium owncloud-client vlc libreoffice-fresh libreoffice-fresh-de hunspell-de\
     gimp inkscape
 
 echo "Generating fstab"
