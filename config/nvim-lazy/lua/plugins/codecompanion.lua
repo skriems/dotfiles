8cfeb36b9965f4414b65858aace1d03652450eaa
Sebastian Kriems <bastoberto@gmx.de>
2025-08-25 15:09:57 +0200

    feat: spinner for codecompanion

--- config/nvim-lazy/lua/plugins/codecompanion.lua
+++ config/nvim-lazy/lua/plugins/codecompanion.lua
@@ -1,21 +1,13 @@
+-- https://github.com/olimorris/dotfiles/blob/main/.config/nvim/lua/plugins/coding.lua
+
 return {
   {
     "olimorris/codecompanion.nvim",
-    opts = {
-      extensions = {
-        mcphub = {
-          callback = "mcphub.extensions.codecompanion",
-          opts = {
-            make_vars = true,
-            make_slash_commands = true,
-            show_result_in_chat = true,
-          },
-        },
-      },
-    },
     dependencies = {
       "nvim-lua/plenary.nvim",
       "nvim-treesitter/nvim-treesitter",
+      -- "j-hui/fidget.nvim", -- Display status,
+      "franco-ruggeri/codecompanion-spinner.nvim", -- progress spinner
       {
         "MeanderingProgrammer/render-markdown.nvim",
         ft = { "markdown", "codecompanion" },
@@ -30,15 +22,71 @@ return {
       --     },
       --   },
       -- },
-      {
-        "echasnovski/mini.diff",
-        config = function()
-          local diff = require("mini.diff")
-          diff.setup({
-            -- Disabled by default
-            source = diff.gen_source.none(),
+      -- {
+      --   "echasnovski/mini.diff",
+      --   config = function()
+      --     local diff = require("mini.diff")
+      --     diff.setup({
+      --       -- Disabled by default
+      --       source = diff.gen_source.none(),
+      --     })
+      --   end,
+      -- },
+    },
+    opts = {
+      ---@module "codecompanion"
+      ---@type CodeCompanion.Config
+      extensions = {
+        mcphub = {
+          callback = "mcphub.extensions.codecompanion",
+          opts = {
+            make_vars = true,
+            make_slash_commands = true,
+            show_result_in_chat = true,
+          },
+        },
+        spinner = {},
+      },
+      adapters = {
+        gemini = function()
+          return require("codecompanion.adapters").extend("gemini", {
+            env = {
+              api_key = "cmd:env | grep GEMINI_API_KEY | awk -F= '{print $2}'",
+            },
           })
         end,
+        ollama = function()
+          return require("codecompanion.adapters").extend("ollama", {
+            schema = {
+              model = {
+                default = "qwen3:latest",
+              },
+              num_ctx = {
+                default = 20000,
+              },
+            },
+          })
+        end,
+        openai = function()
+          return require("codecompanion.adapters").extend("openai", {
+            opts = {
+              stream = true,
+            },
+            env = {
+              api_key = "cmd:env | grep OPENAI_API_KEY | awk -F= '{print $2}'",
+            },
+            schema = {
+              model = {
+                default = function()
+                  return "gpt-5"
+                end,
+              },
+            },
+          })
+        end,
+      },
+      opts = {
+        log_level = "DEBUG",
       },
     },
   },
