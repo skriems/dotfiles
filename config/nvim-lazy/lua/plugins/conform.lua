1e1d980ce8a2700e7f0e01b7e6a8492e93fbb0eb
Sebastian Kriems <bastoberto@gmx.de>
2025-08-29 11:19:52 +0200

    feat: simpler conform config

--- config/nvim-lazy/lua/plugins/conform.lua
+++ config/nvim-lazy/lua/plugins/conform.lua
@@ -1,89 +1,22 @@
-local function find_config(bufnr, config_files)
-  return vim.fs.find(config_files, {
-    upward = true,
-    stop = vim.fs.dirname(vim.api.nvim_buf_get_name(bufnr)),
-    path = vim.fs.dirname(vim.api.nvim_buf_get_name(bufnr)),
-  })[1]
-end
-
-local function get_formatter(bufnr)
-  local has_biome_config = find_config(bufnr, { "biome.json", "biome.jsonc" })
-
-  -- vim.notify("Checking for Biome config: " .. tostring(has_biome_config), vim.log.levels.DEBUG)
-
-  if has_biome_config then
-    return { "biome-check", stop_after_first = true }
-  end
-
-  local has_eslint_config = find_config(bufnr, {
-    "eslint.config.js",
-    "eslint.config.mjs",
-    "eslint.config.cjs",
-    ".eslintrc",
-    ".eslintrc.json",
-    ".eslintrc.yml",
-    ".eslintrc.yaml",
-    ".eslintrc.json5",
-    ".eslintrc.js",
-    ".eslintrc.cjs",
-    ".eslintrc.toml",
-  })
-
-  if has_eslint_config then
-    return { "eslint", stop_after_first = true }
-  end
-
-  local has_prettier_config = find_config(bufnr, {
-    ".prettierrc",
-    ".prettierrc.json",
-    ".prettierrc.yml",
-    ".prettierrc.yaml",
-    ".prettierrc.json5",
-    ".prettierrc.js",
-    ".prettierrc.cjs",
-    ".prettierrc.toml",
-    "prettier.config.js",
-    "prettier.config.cjs",
-  })
-
-  if has_prettier_config then
-    return { "prettier", stop_after_first = true }
-  end
-
-  -- Default to Biome if no config is found
-  return { "biome-check", stop_after_first = true }
-end
-
-local filetypes_with_dynamic_formatter = {
-  "javascript",
-  "javascriptreact",
-  "typescript",
-  "typescriptreact",
-  "vue",
-  "css",
-  "scss",
-  "less",
-  "html",
-  "json",
-  "jsonc",
-  "yaml",
-  "markdown",
-  "markdown.mdx",
-  "graphql",
-  "handlebars",
+local js_linters = {
+  "biome-check",
+  "eslint",
+  "prettierd",
+  stop_after_first = true,
 }
 
 return {
   {
     "stevearc/conform.nvim",
     opts = {
-      formatters_by_ft = (function()
-        local result = {}
-        for _, ft in ipairs(filetypes_with_dynamic_formatter) do
-          result[ft] = get_formatter
-        end
-        return result
-      end)(),
+      formatters_by_ft = {
+        lua = { "stylua" },
+        rust = { "rustfmt", lsp_format = "fallback" },
+        javascript = js_linters,
+        javascriptreact = js_linters,
+        typescript = js_linters,
+        typescriptreact = js_linters,
+      },
     },
   },
 }
